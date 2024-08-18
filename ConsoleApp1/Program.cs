57d63b76358d7860988f3912167356d01d76ba22
Omama <omama@alrsoft.com>
2024-08-18 15:15:18 +0500

    18/08/2024

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 //using System.CodeDom;
 //using System.Collections.Generic;
@@ -16,10 +19,92 @@ namespace ConsoleApp1
         //}
 
 
-        static float calculateAvg(int a, int b, int c)
+        //static float calculateAvg(int a, int b, int c)
+        //{
+        //    float Sum = a + b + c;
+        //    return Sum / 3;
+        //}
+
+        /* ===========================================================================
+                                    Object Oriented Programming in C# 
+         ============================================================================== */
+
+
+        //public class Calculator
+        //{
+        //    public int  num1;
+        //    public int num2;
+        //    private int result;
+
+        //    public void Add()
+        //    {
+        //        result = num1 + num2;
+        //        Result();
+        //    }
+
+
+        //    public void Sub()
+        //    {
+        //        result = num1 - num2;
+        //        Result();
+        //    }
+
+
+        //    public void Mul()
+        //    {
+        //        result = num1 * num2;
+        //        Result();
+        //    }
+
+        //    void Result()
+        //    {
+        //        Console.WriteLine($"Result is : {result}");
+        //        Console.ReadLine();
+        //    }
+
+        //}
+
+
+        //public class Example2
+        //{
+        //    public string[] myArray = {"Omama", "Ali", "Ahmed", "Usama", "Umer", "Muneeb"};
+        //    public int findMax(int num1, int num2)
+        //    {
+        //        int result;
+        //        if (num1 > num2)
+        //        {
+        //            result = num1;
+        //        }
+        //        else
+        //        {
+        //            result = num2;
+        //        }
+        //        return result;
+        //    }
+
+        //    public void printArray()
+        //    {
+        //        for (int i = 0; i < myArray.Length; i++)
+        //        {
+        //            Console.WriteLine(myArray[i]);
+        //        }
+        //    }
+        //}
+
+
+        public class Example3
         {
-            float Sum = a + b + c;
-            return Sum / 3;
+            int num = 1;
+            public void Counting()
+            {
+                if(num == 11)
+                {
+                    return;  // exit from recurion
+                }
+                Console.WriteLine(num);
+                num++;
+                Counting();
+            }
         }
 
         static void Main(string[] args)
@@ -190,7 +275,83 @@ namespace ConsoleApp1
             //}
 
             //Greet();
-            Console.WriteLine(calculateAvg(10, 20, 30));
+            //Console.WriteLine(calculateAvg(10, 20, 30));
+
+
+            /* ===================================
+                Arrays in C# 
+             =================================== */
+
+            //int[] myNum = { 50, 28, 25, 40 };
+            //for(int i = 0; i < myNum.Length; i++)
+            //{
+            //    Console.WriteLine(myNum[i]);
+            //}
+
+            // Add each number of an array and print the result in console.
+            //int sum = 0;
+            //for (int i = 0; i < myNum.Length; i++)
+            //{
+            //    sum = sum + myNum[i];
+            //    Console.WriteLine($"Total is : {sum}");
+            //}
+
+
+            // sorting an array in C# :
+            //There are many array methods available, for example Sort(),
+            //which sorts an array alphabetically or in an ascending order:
+            //Array.Sort(myNum);
+            //foreach (int i in myNum)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            /* ===================================
+                           List in C# 
+               =================================== */
+
+            //List<string> Cars = new List<string>();
+            //Cars.Add("Suzuki");
+            //Cars.Add("Maruti");
+            //Cars.Add("Toyota");
+            //Cars.Add("BMW");
+            //Cars.Add("Ferarri");
+
+            //for(int i = 0; i < Cars.Count; i++)
+            //{
+            //    Console.WriteLine(Cars[i]);
+            //}
+
+            //foreach (string Car in Cars)
+            // {
+            //     Console.WriteLine(Car.ToUpper());
+            // }
+
+
+            /* ===========================================================================
+                                           Object Oriented Programming in C# 
+            ============================================================================== */
+
+            //Here we are creating an object of a class.
+            //Calculator obj = new Calculator(); // using new keyword object gets initialized.
+            //obj.num1 = 50;
+            //obj.num2 = 20;
+
+            //obj.Add();
+            //obj.Sub();
+            //obj.Mul();
+
+            //Example2 example2 = new Example2();
+            //int maxNumber = example2.findMax(10,1);
+            //Console.WriteLine(maxNumber);
+            //example2.printArray();
+
+
+
+            Example3 obj3 = new Example3();
+            obj3.Counting();
             Console.ReadLine();
         }
     }
